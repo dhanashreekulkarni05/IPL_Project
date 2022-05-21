b734903dbb90afeb88fbf911089b406da3573333
Dhanashree Kulkarni <dhanashreekulkarni05@gmail.com>
2022-05-21 16:34:15 +0530

    Reading CSV file

---
+++ IPL_scenarios/src/ReadDeliveries.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ReadDeliveries {
+    public static void main(String[] args)
+    {
+        try(BufferedReader br = new BufferedReader(new FileReader("deliveries.csv")))
+        {
+            List<List<String>> data = new ArrayList<>();
+
+            //Reading until we run out of lines
+            String str="";
+            br.readLine();
+            while((str = br.readLine()) != null)
+            {
+                List<String> current = Arrays.asList(str.split(","));//splitting lines
+
+                data.add(current);
+                str = br.readLine();
+            }
+
+//            //printing the fetched data
+//            for(List<String> list : data)
+//            {
+//                for(String s : list)
+//                    System.out.print(s + " ");
+//                System.out.println();
+//            }
+        }
+        catch(Exception e)
+        {
+            System.out.print(e);
+        }
+    }
+}
